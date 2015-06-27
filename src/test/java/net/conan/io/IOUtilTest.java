9f70877ee2de5241aa83d141b7ec696859d6a354
MadConan <madconan@gmail.com>
2015-06-27 12:55:19 -0400

    Test class for IOUtil

---
+++ src/test/java/net/conan/io/IOUtilTest.java
@@ -0,0 +1,20 @@
+package net.conan.io;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Conan Dombroski (dombroco)
+ */
+public class IOUtilTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testReadWrite() throws Exception {
+
+    }
+}
