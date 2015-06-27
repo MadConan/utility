b997048346a2b46da90a8fe8651a8ae000040aab
MadConan <madconan@gmail.com>
2015-06-27 13:05:06 -0400

    First real commit.

--- src/main/java/net/conan/io/IOUtil.java
+++ src/main/java/net/conan/io/IOUtil.java
@@ -1,8 +1,26 @@
 package net.conan.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * @author Conan Dombroski (dombroco)
  */
-public class IOUtil {
+public final class IOUtil {
+    private static final int BUFFER_SIZE = 1 << 10;
+
+    public static void readWrite(InputStream in, OutputStream out){
+        int bytesRead=0;
+        byte[] bytes = new byte[BUFFER_SIZE];
+        try {
+            while ((bytesRead = in.read(bytes)) != -1) {
+                out.write(bytes,0,bytesRead);
+            }
+        }catch (IOException e){
+            throw new IllegalStateException("Failed to transfer data from in to out.  Current bytesRead="+bytesRead,e);
+        }
+    }
+
 
 }
--- src/test/java/net/conan/io/IOUtilTest.java
+++ src/test/java/net/conan/io/IOUtilTest.java
@@ -1,7 +1,14 @@
 package net.conan.io;
 
+import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * @author Conan Dombroski (dombroco)
@@ -15,6 +22,33 @@ public class IOUtilTest {
 
     @Test
     public void testReadWrite() throws Exception {
+        ByteArrayInputStream in = new ByteArrayInputStream("This is a test".getBytes());
+        ByteArrayOutputStream out = new ByteArrayOutputStream(500);
+        IOUtil.readWrite(in,out);
+        TestCase.assertEquals("This is a test",out.toString());
+    }
 
+    @Test(expected = IllegalStateException.class)
+    public void testReadWriteFailOnRead() throws Exception{
+        InputStream in = new InputStream() {
+            @Override
+            public int read() throws IOException {
+                throw new IOException("TEST");
+            }
+        };
+        ByteArrayOutputStream out = new ByteArrayOutputStream(500);
+        IOUtil.readWrite(in,out);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testReadWriteFailOnWrite() throws Exception {
+        InputStream in = new ByteArrayInputStream("test".getBytes());
+        OutputStream out = new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+                throw new IOException("TEST");
+            }
+        };
+        IOUtil.readWrite(in,out);
     }
 }
