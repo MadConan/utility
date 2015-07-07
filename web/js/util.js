04a4958a8cac725f4d07e4b6c6954b56877ce7bb
MadConan <madconan@gmail.com>
2015-07-07 14:58:32 -0400

    javascript utility

---
+++ web/js/util.js
@@ -0,0 +1,15 @@
+function isThing(thing){
+    return typeof thing !== 'undefined';
+}
+
+function randomInt(){
+    var min = isThing(arguments[0]) ? arguments[0] : 1;
+    var max = isThing(arguments[1]) ? arguments[1] : 900000001;
+    if(max < min){
+        var temp = max;
+        max = min;
+        min = temp;
+    }
+
+    var x = Math.floor(Math.random() * (max - min + 1)) + 1;
+}
